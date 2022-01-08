541d4bc49964c379e154c0fbc76e8b90fa11a305
ANTONIA <geizaaquino@hotmail.com>
2022-01-07 22:17:12 -0300

    First commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,8 +7,46 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
 
         System.out.println(gato);
+
+        livro livro1 = new livro("O problem dos 3 corpos", 300);
+        System.out.println(livro1);
        /* int a = 10;
         int b = 3;
         System.out.println("Hello world " + (a+b));*/
+    }
+}
+class livro{
+    private String nome;
+    private Integer numPagina;
+
+    public livro(String nome, Integer numPagina) {
+        this.nome = nome;
+        this.numPagina = numPagina;
+
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPagina() {
+        return numPagina;
+    }
+
+    public void setNumPagina(Integer numPagina) {
+        this.numPagina = numPagina;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPagina=" + numPagina +
+                '}';
     }
 }
